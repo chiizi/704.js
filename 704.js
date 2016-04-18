7b0ae9d433166bfd4b107c55b8418b8802bf2f39
Hans Felde <m33pfish@gmail.com>
2016-04-18 09:04:17 -0500

    Update 704.js

--- 704.js
+++ 704.js
@@ -1,1 +1,49 @@
-
+const NFPA704 = function(size, flammability, health, instability, special) {
+  return "<svg width=\"" + size + "\" height=\"" + size + "\" xmlns=\"https://www.w3.org/2000/svg\"\n\
+  <style>\n\
+    @import url(\"http://overpass-30e2.kxcdn.com/overpass.css\");\n\
+    .b {\n\
+      width: 10%;\n\
+      height: 10%;\n\
+      background: #000;\n\
+      position: absolute;\n\
+      top: 0;\n\
+      left: 0;\n\
+      transform: rotate(45deg);\n\
+    }\n\
+    .q {\n\
+      width: 6.75%;\n\
+      height: 6.75%;\n\
+      position: absolute;\n\
+      transform: translate(-50%, -50%) rotate(45deg);\n\
+    }\n\
+    .q text {\n\
+      transform: rotate(-45deg);\n\
+    }\n\
+    .red {\n\
+      background: #FF6666;\n\
+      position: absolute;\n\
+      top: 25%;\n\
+      left: 50%;\n\
+    }\n\
+    .blue {\n\
+      background: #6691FF;\n\
+      position: absolute;\n\
+      top: 50%;\n\
+      left: 25%;\n\
+    }\n\
+    .yellow {\n\
+      background: #FCFF66;\n\
+      position: absolute;\n\
+      top: 50%;\n\
+      left: 75%;\n\
+    }\n\
+    .white {\n\
+      background: #FFFFFF;\n\
+      top: 25%;\n\
+      left: 50%;\n\
+    }\n\
+  </style>\n\
+  <polygon  />\n\
+</svg>"
+};
